0eee893da603c9619a6a2df73bcce5deaff4e4e1
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-11-19 15:06:32 +0700

    ss2 bai 6

--- ss2 bai 6.cpp
+++ ss2 bai 6.cpp
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int dodaicanh = 5;
-	
-	// chu vi hinh vuong 
-	int chuvi = dodaicanh * 4;
-	printf("Chu vi hinh vuong %d \n", chuvi);
-	
-	// dien tich hinh vuong
-	int dientich = dodaicanh * dodaicanh;
-	printf("Dien tich hinh vuong %d \n", dientich);
-	return 0; 
+	float r = 2;
+	float pi = 3.14;
+	 // tinh chu vi hinh tron
+	 float chuvi = 2 * pi * r;
+	 printf("Chu vi hinh tron %lf \n", chuvi);
+	 
+	 // tinh dien tich hinh tron 
+	 float dientich = 2 * pi  * r * r;
+	 printf("Dien tich hinh tron %lf \n", dientich); 
+	 return 0; 
  } 
