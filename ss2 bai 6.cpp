c8c1e9cb05849ffd6e85940eb64d7c9c7350f307
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-11-19 14:48:25 +0700

    ss2 bai 6

---
+++ ss2 bai 6.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int dodaicanh = 5;
+	
+	// chu vi hinh vuong 
+	int chuvi = dodaicanh * 4;
+	printf("Chu vi hinh vuong %d", chuvi);
+	
+	// dien tich hinh vuong
+	int dientich = dodaicanh * dodaicanh;
+	printf("Dien tich hinh vuong %d", dientich);
+	return 0; 
+ } 
