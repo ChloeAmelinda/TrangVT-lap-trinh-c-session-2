f25a3ed9599920d5ae755e4eb9b4201528ca6f7a
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-11-19 15:13:27 +0700

    ss2 bai 3

---
+++ ss2 bai 3.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int so1 = 20;
+	int so2 = 4;
+	
+	// toan tu +
+	int tong = so1 + so2;
+	printf("Phep tong la %d \n",tong);
+	
+	// toan tu -
+	int hieu = so1 - so2;
+	printf("Phep tong la %d \n",hieu);
+	
+	// toan tu *
+	 int tich = so1 * so2;
+	 printf("Phep nhan la %d \n",tich);
+	 
+	 // toan tu /
+	 int thuong = so1 / so2;
+	 printf("Phep chia la %d \n",thuong); 
+	 
+	 // toan tu chia lay du %
+	 int chialaydu = so1 % so2;
+	 printf("Phep chia lay du %d \n",chialaydu); 
+		return 0; 
+	 
+ } 
