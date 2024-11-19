c165874fda42d2bb68953cd5c2b1483d40ab056b
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-11-19 14:40:52 +0700

    ss2 bai 5

---
+++ ss2 bai 5.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{ 
+ int dai = 4;
+ int rong = 3;
+ // tinh chu vi 
+ int chuvi = 2 * (dai + rong);
+ printf("Chu vi %d", chuvi);
+ 
+ // tinh dien tich 
+ int dientich = dai * rong;
+ printf("Dien tich %d", dientich);
+ return 0; 
+ } 
